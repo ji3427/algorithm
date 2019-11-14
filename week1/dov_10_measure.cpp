97006327d70e0c58f705f78765726603758f8252
jihwayeong <ji5ji@naver.com>
2019-11-14 19:17:53 +0900

    feat : solve week1 fail problem

--- week1/dov_10_measure.cpp
+++ week1/dov_10_measure.cpp
@@ -1,5 +1,44 @@
-//못풀었다...
+#include <iostream>
+#include <algorithm>
 
-문제 : http://59.23.150.58/30stair/measure/measure.php?pname=measure
+using namespace std;
 
-답 : https://dyndy.tistory.com/228
+int point[3][2];
+int main(void)
+{
+  double length;
+  cin>>length;
+  double mid;
+  for(int i = 0 ; i < 3; i ++){
+    cin>>point[i][0]>>point[i][1];
+  }
+  for(int i = 0 ; i < 3; i++){
+    if(point[i][0] != point[i][1]){
+      mid = (double)(point[i][0] + point[i][1]) / 2;
+      if(mid < length/2){
+        for(int j = i + 1; j < 3 ; j++){
+          for(int k = 0 ; k < 2 ; k++){
+            if(point[j][k] <= mid){
+              point[j][k] += (length - mid * 2);
+            }
+            else{
+              point[j][k] = length - point[j][k]; 
+            }
+          }
+        }
+        length-= mid;
+      }
+      else{
+        for(int j = i + 1 ; j < 3 ; j++){
+          for(int k = 0 ; k < 2 ; k++){
+            if(point[j][k] > mid)
+              point[j][k] = mid - (point[j][k] - mid);
+          }
+        }
+        length = mid;
+      }
+    }
+  }
+  printf("%0.1f", length);
+  return 0;
+}
